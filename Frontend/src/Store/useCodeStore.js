1d448d40067bbc18e0e4cd51540036eedbe2c56e
Manohar Kumar <icsixyz@gmail.com>
2025-02-15 19:24:24 +0530

    api call handling

--- Frontend/src/Store/useCodeStore.js
+++ Frontend/src/Store/useCodeStore.js
@@ -1,5 +1,6 @@
 import { create } from "zustand";
-import axios from "axios"
+import { axiosInstance } from "../lib/axios";
+import axios from "axios";
 
 export const useCodeStore = create((set) => ({
     reviewedCode: null,
@@ -8,9 +9,8 @@ export const useCodeStore = create((set) => ({
     setReviewedCode: async (code) => {
         set({ isLoadingReview: true });
         try {
-            const response = await axios.post("http://localhost:8000/api/get-review", {code});
+            const response = await axiosInstance.post("/get-review", {code}) ;
             set({ reviewedCode: response.data })
-            console.log(response.data)
         }
         catch (error) {
             console.log(error);
