f67f44226a6c2067f3037c8317b1e1173b02e70b
Manohar Kumar <icsixyz@gmail.com>
2025-06-02 18:58:47 +0530

    removed dummy data

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import AIRoute from "./routes/ai.routes.js";
-import dataRoute from "./routes/data.route.js";
 import cors from "cors"
 import path from "path"
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT ;
-import {data} from "./MOCK_DATA.js" 
-import {USER} from "./MOCK_DATA2.js" 
+
 import connectDB from "./Config/mongodb.js";
 
 app.use(express.json());
@@ -27,7 +25,6 @@ app.get("/api/data" , (__ , res ) => {
 const __dirname = path.resolve();
 
     app.use("/api" , AIRoute);
-    app.use("/api" , dataRoute);
 
     app.use(express.static(path.join(__dirname,"../Frontend/dist")));
 
